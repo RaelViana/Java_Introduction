d1ec7734f0b71238f36bc46b4d2c6ac8fd388985
RaelViana <catedralviana@hotmail.com>
2021-07-18 14:31:29 -0300

    Variaveis - tipos

---
+++ Variaveis.java
@@ -0,0 +1,24 @@
+public class Variaveis {
+	
+	/*
+	 Aplicação de Variáveis mais comuns da linguagem
+	 
+	*/
+	
+	
+	
+	public static void main(String[] args){
+	
+		String nome = "Linus Torvalds"; //Variável de Textos
+		int numero = 17;                //Variável de numeros inteiros		
+		double d = 1.75 ;               //Variável de Numeros inteiros e fracionados
+		boolean log = true;		        //Operador lógico retorna verdadeiro ou falso
+		char sexo = 'M';
+		
+		System.out.println(nome);        // Imprime o valor da variável
+		System.out.println(numero);
+		System.out.println(d);
+		System.out.println(log);
+		System.out.println(sexo);
+	}
+}
