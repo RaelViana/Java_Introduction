94c537bc1dc176e346e8621a83c92dd193d87532
RaelViana <catedralviana@hotmail.com>
2021-07-22 20:50:55 -0300

    Operadores- Binário, Unário, Ternário

---
+++ Operador.java
@@ -0,0 +1,19 @@
+/*
+	Operadores - binário,unário e ternário
+*/
+
+ public class Operador {
+	
+	public static void main(String[] args){
+	
+		int soma = 1 + 3;   //exemplo de operador binário
+		System.out.println(soma);
+		
+		int num = -1 ; //exemplo de operador Unário
+		System.out.println(num);
+		
+		boolean b = true ? "sim" : "não";  //exemplo de operador ternário
+		
+	}
+	
+}
