3987b74179186a3d2d7ad7f94b5aad4a2c1b5b90
RaelViana <catedralviana@hotmail.com>
2021-07-29 20:37:48 -0300

    Operadores de Comparação

---
+++ OperadoresComparacao.java
@@ -0,0 +1,30 @@
+/*
+	Operadores - comparação
+*/
+
+ public class OperadoresComparacao {
+	
+	public static void main(String[] args){
+	
+		int x = 3;   //variavel x recebe valor '3'
+		System.out.println(x == 3); //verifica se o valor é igual(retorna true)
+		
+		int y = 2;   //variavel y recebe valor '2'
+		System.out.println(y != 2); //verifica se o valor é diferente(retorna false)
+		
+		int z = 5;   //variavel 5 recebe valor '5'
+		System.out.println(z > 2); //verifica se o valor é maior(retorna true)
+		
+		int w = 7;   //variavel w recebe valor '7'
+		System.out.println(w < 7); //verifica se o valor é menor(retorna false)
+		
+		int r = 13;   //variavel r recebe valor '13'
+		System.out.println(r <= 7); //verifica se o valor é menor ou igual(retorna false)
+		
+		
+		int g = 7;   //variavel g recebe valor '1'
+		System.out.println(g >= 7); //verifica se o valor é maior ou igual(retorna true)
+		
+	}
+	
+}
