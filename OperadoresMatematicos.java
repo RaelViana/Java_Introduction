8e6d9c7428cec6f6d3d9996e2a767e9057685cd5
RaelViana <catedralviana@hotmail.com>
2021-07-29 20:20:33 -0300

    Operadores Matemáticos

---
+++ OperadoresMatematicos.java
@@ -0,0 +1,38 @@
+/*
+	Operadores - Matemáticos
+*/
+
+ public class OperadoresMatematicos {
+	
+	public static void main(String[] args){
+	
+		int soma = 1 + 3;   //exemplo de operador soma
+		System.out.println(soma);
+		
+		int subtracao = 7 - 5;   //exemplo de operador subtração
+		System.out.println(subtracao);
+		
+		int multiplicacao = 5 * 3;   //exemplo de operador multiplicacao
+		System.out.println(multiplicacao);
+		
+		int divisao = 27 / 9 ; //exemplo de operador divisao
+		System.out.println(divisao);
+		
+		int modulo = 10 % 3 ; //exemplo de operador modulo- apresenta o resto da divisão entre dois numeros
+		System.out.println(modulo);
+		
+		int x = 5;   //exemplo de operador de incremento
+		int y = ++x; //incrementa o valor de 'X' mais 1
+		System.out.println(x);
+		
+		int a = 3;   //exemplo de operador multiplicacao
+		int b = --a;
+		System.out.println(a);
+		
+		
+		
+		
+		
+	}
+	
+}
