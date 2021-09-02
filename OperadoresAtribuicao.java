f6d317fef4738cbcdf8a81402c47c77e7db198b9
RaelViana <catedralviana@hotmail.com>
2021-09-02 19:20:24 -0300

    Operadores de Atribuição

---
+++ OperadoresAtribuicao.java
@@ -0,0 +1,37 @@
+/*
+	Operadores - Atribuição
+	
+	+= Adição
+	-= Subtração
+	*= Multiplicação
+	/= Divisão
+	%= módulo
+	
+*/
+
+ public class OperadoresAtribuicao {
+	
+	public static void main(String[] args){
+		
+		int x = 8;
+		
+		x += 5; //atribui + 5 ao valor de x
+		
+		x -= 3; //subtrai 3 ao valor de x
+		
+		x *= 4; // mutiplica por 4 valor de x
+		
+		x /= 2; // divide por 2 o valor  de x
+		
+		x %= 1; //apreseta o resto da divisão de x por 1
+		
+		
+		
+		System.out.println(x); //retorna valor de x
+		
+		
+	
+		
+	}
+	
+}
